b7ba9ed042842f83426405a640d5777f2636fc91
Nick Bronn <nick.bronn@gmail.com>
2022-02-15 18:55:47 -0500

    added comments and now allow for input to be list of quantumcircuits as well

--- qiskit_research/utils/dynamical_decoupling.py
+++ qiskit_research/utils/dynamical_decoupling.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-from typing import Optional, Union
+from typing import List, Optional, Union
 from qiskit.qasm import pi
 import numpy
 
@@ -18,21 +18,34 @@ from qiskit import pulse
 from qiskit.circuit import QuantumCircuit
 from qiskit.circuit.gate import Gate
 from qiskit.circuit.library import XGate, YGate
+from qiskit.providers import BaseBackend
+from qiskit.providers.backend import Backend
 from qiskit.pulse import DriveChannel
 from qiskit.transpiler import InstructionDurations, PassManager
 from qiskit.transpiler.passes import ALAPSchedule, ASAPSchedule, DynamicalDecoupling
 
-def add_dd_calibrations(qc: QuantumCircuit, backend, dd_str: str, sched_method=None):
+def add_dd_calibrations(
+    circuits: Union[QuantumCircuit, List[QuantumCircuit]],
+    backend: Union[Backend, BaseBackend],
+    dd_str: str,
+    sched_method: Optional[str] = None,
+) -> Union[QuantumCircuit, List[QuantumCircuit]]:
+    """
+    Add dynamical decoupling sequences and the calibrations necessary
+    to run them on an IBM backend.
+    """
+
     if sched_method is None:
         sched_method = 'alap'
 
-    qc_dd = add_dd_sequence(qc, backend, dd_str, sched_method)
-    add_dd_pulse_calibrations(qc_dd, backend)
+    circuits_dd = add_dd_sequence(circuits, backend, dd_str, sched_method)
+    add_dd_pulse_calibrations(circuits_dd, backend)
 
-    return qc_dd
+    return circuits_dd
 
-def get_dd_sequence(dd_str: str): # -> List(Gate):
-    r"""
+def get_dd_sequence(dd_str: str) -> List[Gate]:
+    """
+    Returns dynamical decoupling sequence based on input string.
     We will define standard dynamical decoupling sequences via the
     strings:
 
@@ -59,7 +72,21 @@ def get_dd_sequence(dd_str: str): # -> List(Gate):
     else:
         return []
 
+
+
 def get_timing(backend) -> InstructionDurations:
+    """
+    Retrieves gate timing information for the backend from the instruction
+    schedule map, and returns the type InstructionDurations for use by
+    Qiskit's scheduler (i.e., ALAP) and DynamicalDecoupling passes.
+
+    This method relies on IBM backend knowledge such as
+
+      - all single qubit gates durations are the same
+      - the 'x' gate, used for echoed cross resonance, is also the basis for
+        all othe dynamical decoupling gates (currently)
+    """
+
     inst_durs = []
     inst_sched_map = backend.defaults().instruction_schedule_map
     num_qubits = backend.configuration().num_qubits
@@ -87,7 +114,19 @@ def get_timing(backend) -> InstructionDurations:
 
     return InstructionDurations(inst_durs)
 
-def add_dd_sequence(qc: QuantumCircuit, backend, dd_str: str, sched_method: str) -> QuantumCircuit:
+
+
+def add_dd_sequence(
+    circuits: Union[QuantumCircuit, List[QuantumCircuit]],
+    backend: Union[Backend, BaseBackend],
+    dd_str: str,
+    sched_method: str,
+) -> Union[QuantumCircuit, List[QuantumCircuit]]:
+    """
+    Schedules the circuit accoring to sched_method string, followed by
+    inserting the dynamical decoupling sequence into the circuit(s).
+    """
+
     durations = get_timing(backend)
     sequence = get_dd_sequence(dd_str)
 
@@ -98,11 +137,23 @@ def add_dd_sequence(qc: QuantumCircuit, backend, dd_str: str, sched_method: str)
         pm = PassManager([ALAPSchedule(durations),
             DynamicalDecoupling(durations, sequence)])
 
-    return pm.run(qc)
+    return pm.run(circuits)
 
-def add_dd_pulse_calibrations(qc: QuantumCircuit, backend):
+
+
+def add_dd_pulse_calibrations(
+    circuits: Union[QuantumCircuit, List[QuantumCircuit]],
+    backend: Union[Backend, BaseBackend],
+):
+    """
+    Build the pulse schedule and attach as pulse gate to all the
+    gates used for dynamical decoupling.
+    """
     inst_sched_map = backend.defaults().instruction_schedule_map
     num_qubits = backend.configuration().num_qubits
+
+    if isinstance(circuits, QuantumCircuit):
+        circuits = [circuits]
 
     for qubit in range(num_qubits):
         with pulse.build('xp gate for qubit '+str(qubit)) as sched:
@@ -111,7 +162,7 @@ def add_dd_pulse_calibrations(qc: QuantumCircuit, backend):
             pulse.call(x_sched)
 
             # for each DD sequence with a XpGate() in it
-            for circ in [qc]:
+            for circ in circuits:
                 circ.add_calibration('xp', [qubit], sched)
 
         with pulse.build('xm gate for qubit '+str(qubit)) as sched:
@@ -122,7 +173,7 @@ def add_dd_pulse_calibrations(qc: QuantumCircuit, backend):
             pulse.play(x_pulse, DriveChannel(qubit))
 
             # for each DD sequence with a XmGate() in it
-            for circ in [qc]:
+            for circ in circuits:
                 circ.add_calibration('xm', [qubit], sched)
 
         with pulse.build('y gate for qubit '+str(qubit)) as sched:
@@ -132,7 +183,7 @@ def add_dd_pulse_calibrations(qc: QuantumCircuit, backend):
                 pulse.call(x_sched)
 
             # for each DD sequence with a YGate() in it
-            for circ in [qc]:
+            for circ in circuits:
                 circ.add_calibration('y', [qubit], sched)
 
         with pulse.build('yp gate for qubit '+str(qubit)) as sched:
@@ -142,7 +193,7 @@ def add_dd_pulse_calibrations(qc: QuantumCircuit, backend):
                 pulse.call(x_sched)
 
             # for each DD sequence with a YpGate() in it
-            for circ in [qc]:
+            for circ in circuits:
                 circ.add_calibration('yp', [qubit], sched)
 
         with pulse.build('ym gate for qubit '+str(qubit)) as sched:
@@ -154,8 +205,10 @@ def add_dd_pulse_calibrations(qc: QuantumCircuit, backend):
                 pulse.play(x_pulse, DriveChannel(qubit))
 
             # for each DD sequence with a YmGate() in it
-            for circ in [qc]:
+            for circ in circuits:
                 circ.add_calibration('ym', [qubit], sched)
+
+
 
 class XpGate(Gate):
     r"""The single-qubit Pauli-X gate (:math:`\sigma_x`), implemented
