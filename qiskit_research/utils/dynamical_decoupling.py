0cf222441a9d54b063f9b9d153d6c49390002c2b
Kevin J. Sung <kevjsung@umich.edu>
2022-02-15 09:34:08 -0500

    run black formatter on dynamical decoupling file

--- qiskit_research/utils/dynamical_decoupling.py
+++ qiskit_research/utils/dynamical_decoupling.py
@@ -22,16 +22,18 @@ from qiskit.pulse import DriveChannel
 from qiskit.transpiler import InstructionDurations, PassManager
 from qiskit.transpiler.passes import ALAPSchedule, ASAPSchedule, DynamicalDecoupling
 
+
 def add_dd_calibrations(qc: QuantumCircuit, backend, dd_str: str, sched_method=None):
     if sched_method is None:
-        sched_method = 'alap'
+        sched_method = "alap"
 
     qc_dd = add_dd_sequence(qc, backend, dd_str, sched_method)
     add_dd_pulse_calibrations(qc_dd, backend)
 
     return qc_dd
 
-def get_dd_sequence(dd_str: str): # -> List(Gate):
+
+def get_dd_sequence(dd_str: str):  # -> List(Gate):
     r"""
     We will define standard dynamical decoupling sequences via the
     strings:
@@ -44,20 +46,30 @@ def get_dd_sequence(dd_str: str): # -> List(Gate):
         'XY8pm': Xp-Yp-Xm-Ym-Ym-Xm-Yp-Xp
     """
 
-    if dd_str == 'X2':
+    if dd_str == "X2":
         return [XGate(), XGate()]
-    elif dd_str == 'X2pm':
+    elif dd_str == "X2pm":
         return [XpGate(), XmGate()]
-    elif dd_str == 'XY4':
+    elif dd_str == "XY4":
         return [XGate(), YGate(), XGate(), YGate()]
-    elif dd_str == 'XY4pm':
+    elif dd_str == "XY4pm":
         return [XpGate(), YpGate(), XmGate(), YmGate()]
-    elif dd_str == 'XY8':
+    elif dd_str == "XY8":
         return [XGate(), YGate(), XGate(), YGate(), YGate(), XGate(), YGate(), XGate()]
-    elif dd_str == 'XY8pm':
-        return [XpGate(), YpGate(), XmGate(), YmGate(), YmGate(), XmGate(), YpGate(), XpGate()]
+    elif dd_str == "XY8pm":
+        return [
+            XpGate(),
+            YpGate(),
+            XmGate(),
+            YmGate(),
+            YmGate(),
+            XmGate(),
+            YpGate(),
+            XpGate(),
+        ]
     else:
         return []
+
 
 def get_timing(backend) -> InstructionDurations:
     inst_durs = []
@@ -71,12 +83,12 @@ def get_timing(backend) -> InstructionDurations:
             inst_durs.append((inst_str, qubit, inst.duration))
 
             # create DD pulses from CR echo 'x' pulse
-            if inst_str == 'x':
-                inst_durs.append(('xp', qubit, inst.duration))
-                inst_durs.append(('xm', qubit, inst.duration))
-                inst_durs.append(('y', qubit, inst.duration))
-                inst_durs.append(('yp', qubit, inst.duration))
-                inst_durs.append(('ym', qubit, inst.duration))
+            if inst_str == "x":
+                inst_durs.append(("xp", qubit, inst.duration))
+                inst_durs.append(("xm", qubit, inst.duration))
+                inst_durs.append(("y", qubit, inst.duration))
+                inst_durs.append(("yp", qubit, inst.duration))
+                inst_durs.append(("ym", qubit, inst.duration))
 
     # two qubit gates
     for qc in range(num_qubits):
@@ -87,75 +99,82 @@ def get_timing(backend) -> InstructionDurations:
 
     return InstructionDurations(inst_durs)
 
-def add_dd_sequence(qc: QuantumCircuit, backend, dd_str: str, sched_method: str) -> QuantumCircuit:
+
+def add_dd_sequence(
+    qc: QuantumCircuit, backend, dd_str: str, sched_method: str
+) -> QuantumCircuit:
     durations = get_timing(backend)
     sequence = get_dd_sequence(dd_str)
 
-    if sched_method == 'asap':
-        pm = PassManager([ASAPSchedule(durations),
-            DynamicalDecoupling(durations, sequence)])
-    elif sched_method == 'alap':
-        pm = PassManager([ALAPSchedule(durations),
-            DynamicalDecoupling(durations, sequence)])
+    if sched_method == "asap":
+        pm = PassManager(
+            [ASAPSchedule(durations), DynamicalDecoupling(durations, sequence)]
+        )
+    elif sched_method == "alap":
+        pm = PassManager(
+            [ALAPSchedule(durations), DynamicalDecoupling(durations, sequence)]
+        )
 
     return pm.run(qc)
+
 
 def add_dd_pulse_calibrations(qc: QuantumCircuit, backend):
     inst_sched_map = backend.defaults().instruction_schedule_map
     num_qubits = backend.configuration().num_qubits
 
     for qubit in range(num_qubits):
-        with pulse.build('xp gate for qubit '+str(qubit)) as sched:
+        with pulse.build("xp gate for qubit " + str(qubit)) as sched:
             # def of XpGate() in terms of XGate()
-            x_sched = inst_sched_map.get('x', qubits=[qubit])
+            x_sched = inst_sched_map.get("x", qubits=[qubit])
             pulse.call(x_sched)
 
             # for each DD sequence with a XpGate() in it
             for circ in [qc]:
-                circ.add_calibration('xp', [qubit], sched)
+                circ.add_calibration("xp", [qubit], sched)
 
-        with pulse.build('xm gate for qubit '+str(qubit)) as sched:
+        with pulse.build("xm gate for qubit " + str(qubit)) as sched:
             # def of XmGate() in terms of XGate() and amplitude inversion
-            x_sched = inst_sched_map.get('x', qubits=[qubit])
+            x_sched = inst_sched_map.get("x", qubits=[qubit])
             x_pulse = x_sched.instructions[0][1].pulse
-            x_pulse._amp = -x_pulse.amp # bad form
+            x_pulse._amp = -x_pulse.amp  # bad form
             pulse.play(x_pulse, DriveChannel(qubit))
 
             # for each DD sequence with a XmGate() in it
             for circ in [qc]:
-                circ.add_calibration('xm', [qubit], sched)
+                circ.add_calibration("xm", [qubit], sched)
 
-        with pulse.build('y gate for qubit '+str(qubit)) as sched:
+        with pulse.build("y gate for qubit " + str(qubit)) as sched:
             # def of YGate() in terms of XGate() and phase_offset
-            with pulse.phase_offset(pi/2, DriveChannel(qubit)):
-                x_sched = inst_sched_map.get('x', qubits=[qubit])
+            with pulse.phase_offset(pi / 2, DriveChannel(qubit)):
+                x_sched = inst_sched_map.get("x", qubits=[qubit])
                 pulse.call(x_sched)
 
             # for each DD sequence with a YGate() in it
             for circ in [qc]:
-                circ.add_calibration('y', [qubit], sched)
+                circ.add_calibration("y", [qubit], sched)
 
-        with pulse.build('yp gate for qubit '+str(qubit)) as sched:
+        with pulse.build("yp gate for qubit " + str(qubit)) as sched:
             # def of YpGate() in terms of XGate() and phase_offset
-            with pulse.phase_offset(pi/2, DriveChannel(qubit)):
-                x_sched = inst_sched_map.get('x', qubits=[qubit])
+            with pulse.phase_offset(pi / 2, DriveChannel(qubit)):
+                x_sched = inst_sched_map.get("x", qubits=[qubit])
                 pulse.call(x_sched)
 
             # for each DD sequence with a YpGate() in it
             for circ in [qc]:
-                circ.add_calibration('yp', [qubit], sched)
+                circ.add_calibration("yp", [qubit], sched)
 
-        with pulse.build('ym gate for qubit '+str(qubit)) as sched:
+        with pulse.build("ym gate for qubit " + str(qubit)) as sched:
             # def of YGate() in terms of XGate() and phase_offset
-            with pulse.phase_offset(-pi/2, DriveChannel(qubit)):
-                x_sched = inst_sched_map.get('x', qubits=[qubit])
+            with pulse.phase_offset(-pi / 2, DriveChannel(qubit)):
+                x_sched = inst_sched_map.get("x", qubits=[qubit])
                 x_pulse = x_sched.instructions[0][1].pulse
-                x_pulse._amp = -x_pulse.amp # bad form
+                x_pulse._amp = -x_pulse.amp  # bad form
                 pulse.play(x_pulse, DriveChannel(qubit))
 
             # for each DD sequence with a YmGate() in it
             for circ in [qc]:
-                circ.add_calibration('ym', [qubit], sched)
+                circ.add_calibration("ym", [qubit], sched)
+
 
 class XpGate(Gate):
     r"""The single-qubit Pauli-X gate (:math:`\sigma_x`), implemented
@@ -190,6 +209,7 @@ class XpGate(Gate):
         """Return a numpy.array for the Xp gate."""
         return numpy.array([[0, 1], [1, 0]], dtype=dtype)
 
+
 class XmGate(Gate):
     r"""The single-qubit Pauli-X gate (:math:`\sigma_x`), implemented
     via RX(-\pi).
@@ -223,6 +243,7 @@ class XmGate(Gate):
         """Return a numpy.array for the X gate."""
         return numpy.array([[0, 1], [1, 0]], dtype=dtype)
 
+
 class YpGate(Gate):
     r"""The single-qubit Pauli-Y gate (:math:`\sigma_y`), implemented
     via RY(\pi).
@@ -252,6 +273,7 @@ class YpGate(Gate):
     def __array__(self, dtype=None):
         """Return a numpy.array for the Yp gate."""
         return numpy.array([[0, -1j], [1j, 0]], dtype=dtype)
+
 
 class YmGate(Gate):
     r"""The single-qubit Pauli-Y gate (:math:`\sigma_y`), implemented
