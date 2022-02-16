ff8c13cbc3c749d16886a2854c78bd74cbc9e4e2
Nick Bronn <nick.bronn@gmail.com>
2022-02-15 19:07:46 -0500

    fixed last few errors running through dd example notebook

--- qiskit_research/utils/dynamical_decoupling.py
+++ qiskit_research/utils/dynamical_decoupling.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-from typing import List, Optional, TYPE_CHECKING, Union
+from typing import Iterable, List, Optional, TYPE_CHECKING, Union
 from qiskit.qasm import pi
 import numpy
 
