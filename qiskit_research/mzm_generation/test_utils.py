fix fidelity witness variance

qiskit_research/mzm_generation/test_utils.py
@@ -12,12 +12,16 @@
 
 """Test mzm_generation utils."""
 
+from typing import Optional
 import unittest
 
 import numpy as np
 from qiskit.providers.aer import AerSimulator
 from qiskit.quantum_info import random_hermitian, random_statevector
-from qiskit_nature.operators.second_quantization import QuadraticHamiltonian
+from qiskit_nature.operators.second_quantization import (
+    FermionicOp,
+    QuadraticHamiltonian,
+)
 from qiskit_research.mzm_generation.experiment import (
     CircuitParameters,
     KitaevHamiltonianExperiment,
@@ -29,8 +33,11 @@ from qiskit_research.mzm_generation.utils import (
     covariance_matrix,
     expectation,
     expectation_from_correlation_matrix,
+    fidelity_witness,
     jordan_wigner,
     kitaev_hamiltonian,
+    number_op,
+    _CovarianceDict,
 )
 
 
@@ -39,8 +46,23 @@ def _random_antisymmetric(dim: int):
     return mat - mat.T
 
 
+def _fidelity_witness_alt(
+    corr: np.ndarray, corr_target: np.ndarray, cov: Optional[_CovarianceDict] = None
+) -> float:
+    m, _ = corr.shape
+    n = m // 2
+    Tt = corr_target[:n, :n]
+    St = corr_target[:n, n:]
+    constant = np.trace(2 * Tt @ Tt - Tt - St @ St.conj() - St.conj() @ St)
+    hermitian_part = np.eye(n) - 2 * Tt.conj()
+    antisymmetric_part = -2 * St.conj()
+    op = QuadraticHamiltonian(hermitian_part, antisymmetric_part)
+    exp, std = expectation_from_correlation_matrix(op, corr, cov)
+    return 1 - exp - constant, std
+
+
 class TestMZMGenerationUtils(unittest.TestCase):
-    """Test PhasedXXMinusYYGate."""
+    """Test MZM utils."""
 
     def test_covariance_matrix(self):
         dim = 5
@@ -144,7 +166,24 @@ class TestMZMGenerationUtils(unittest.TestCase):
             chemical_potential=chemical_potential,
         )
         hamiltonian = quad_ham._fermionic_op()
-        exp1, var1 = expectation_from_correlation_matrix(quad_ham, corr, cov)
-        exp2, var2 = expectation_from_correlation_matrix(hamiltonian, corr, cov)
+        exp1, std1 = expectation_from_correlation_matrix(quad_ham, corr, cov)
+        exp2, std2 = expectation_from_correlation_matrix(hamiltonian, corr, cov)
         np.testing.assert_allclose(exp1, exp2, atol=1e-8)
-        np.testing.assert_allclose(var1, var2, atol=1e-8)
+        np.testing.assert_allclose(std1, std2, atol=1e-8)
+
+        # test fidelity witness
+        transformation_matrix, _, _ = quad_ham.diagonalizing_bogoliubov_transform()
+        W1 = transformation_matrix[:, : experiment.n_modes]
+        W2 = transformation_matrix[:, experiment.n_modes :]
+        full_transformation_matrix = np.block([[W1, W2], [W2.conj(), W1.conj()]])
+        occupation = np.zeros(n_modes)
+        occupation[list(occupied_orbitals)] = 1.0
+        corr_diag = np.diag(np.concatenate([occupation, 1 - occupation]))
+        corr_exact = (
+            full_transformation_matrix.T.conj() @ corr_diag @ full_transformation_matrix
+        )
+
+        val1, std1 = fidelity_witness(corr, corr_exact, cov)
+        val2, std2 = _fidelity_witness_alt(corr, corr_exact, cov)
+        np.testing.assert_allclose(val1, val2, atol=1e-8)
+        np.testing.assert_allclose(std1, std2, atol=1e-8)

qiskit_research/mzm_generation/utils.py
@@ -211,36 +211,34 @@ def fidelity_witness(
             for j in range(i + 1, n):
                 for k in range(n):
                     for ell in range(k + 1, n):
-                        var += 2 * np.real(
-                            corr_target[i, j].conjugate()
-                            * corr_target[k, ell].conjugate()
-                            * cov[frozenset([(i, j), (k, ell)])]
-                        )
-                        var += 2 * np.real(
-                            corr_target[i, j].conjugate()
+                        var += 8 * np.real(
+                            corr_target[i, j]
                             * corr_target[k, ell]
                             * cov[frozenset([(i, j), (k, ell)])]
                         )
-                        var += 2 * np.real(
-                            corr_target[i, j + n].conjugate()
-                            * corr_target[k, ell + n].conjugate()
+                        var += 8 * np.real(
+                            corr_target[i, j]
+                            * corr_target[k, ell].conjugate()
+                            * cov[frozenset([(i, j), (k, ell)])]
+                        )
+                        var += 8 * np.real(
+                            corr_target[i, j + n]
+                            * corr_target[k, ell + n]
                             * cov[frozenset([(i, j + n), (k, ell + n)])]
                         )
-                        var += 2 * np.real(
-                            corr_target[i, j + n].conjugate()
-                            * corr_target[k, ell + n]
+                        var += 8 * np.real(
+                            corr_target[i, j + n]
+                            * corr_target[k, ell + n].conjugate()
                             * cov[frozenset([(i, j + n), (k, ell + n)])]
                         )
         # diagonal entries
         for i in range(n):
             for j in range(i, n):
                 var += (1 + (i != j)) * (
-                    (corr_target[i, i] - float(i == j))
-                    * (corr_target[j, j] - float(i == j))
+                    (1 - 2 * corr_target[i, i])
+                    * (1 - 2 * corr_target[j, j])
                     * cov[frozenset([(i, i), (j, j)])]
                 )
-        # account for lower half of correlation matrices
-        var *= 4
 
     return np.real(witness), np.sqrt(np.real(var))
 