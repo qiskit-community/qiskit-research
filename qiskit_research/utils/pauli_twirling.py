dd6f007f6204cffc2b532d502408982da6c26f03
Kevin J. Sung <kevjsung@umich.edu>
2022-03-11 16:17:11 -0500

    twirl all gates, support non-symmetric twirls, remove pulse calibration

--- qiskit_research/utils/pauli_twirling.py
+++ qiskit_research/utils/pauli_twirling.py
@@ -10,117 +10,83 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-from copy import deepcopy
-from typing import Iterable, List, Optional, TYPE_CHECKING, Union
-from qiskit.qasm import pi
-import numpy as np
+from typing import Any, List, Union, cast
 
-from qiskit import pulse
+import numpy as np
 from qiskit.circuit import QuantumCircuit, QuantumRegister
-from qiskit.circuit.gate import Gate
-from qiskit.circuit.library import IGate, XGate, YGate, ZGate, RZXGate
+from qiskit.circuit.library import IGate, XGate, YGate, ZGate
 from qiskit.converters import circuit_to_dag, dag_to_circuit
 from qiskit.dagcircuit import DAGCircuit
-from qiskit.exceptions import QiskitError
-from qiskit.providers.ibmq import IBMQBackend
-from qiskit.pulse import DriveChannel
-from qiskit.quantum_info import Operator
 
-if TYPE_CHECKING:
-    from qiskit.transpiler.basepasses import BasePass
+I = IGate()
+X = XGate()
+Y = YGate()
+Z = ZGate()
 
 TWIRL_GATES = {
-    "rzx": [[IGate(), IGate()], [XGate(), ZGate()],
-        [YGate(), YGate()], [ZGate(), XGate()]],
-    "rzz": [[IGate(), IGate()], [XGate(), XGate()],
-        [YGate(), YGate()], [ZGate(), ZGate()]],
+    "rzx": (
+        ((I, I), (I, I)),
+        ((X, Z), (X, Z)),
+        ((Y, Y), (Y, Y)),
+        ((Z, X), (Z, X)),
+    ),
+    "rzz": (
+        ((I, I), (I, I)),
+        ((X, X), (X, X)),
+        ((Y, Y), (Y, Y)),
+        ((Z, Z), (Z, Z)),
+    ),
 }
+
+
+def parse_random_seed(seed: Any) -> np.random.Generator:
+    """Parse a random number generator seed and return a Generator."""
+    if isinstance(seed, np.random.Generator):
+        return cast(np.random.Generator, seed)
+    return np.random.default_rng(seed)
+
 
 def add_pauli_twirls(
     circuits: Union[QuantumCircuit, List[QuantumCircuit]],
-    backend: IBMQBackend,
-    entangler_str: str,
-    num_twirled_cicuits: int,
-    seed: Union[int, List[int]],
-) -> List[QuantumCircuit]:
-    """
-    Add pairs of gates before/after entangling gate randomly
-    such that they commute. This helps turn coherent error into stochastic
-    error.
+    num_twirled_circuits: int = 1,
+    seed: Any = None,
+) -> Union[List[QuantumCircuit], List[List[QuantumCircuit]]]:
+    """Add Pauli twirls to circuits.
 
     Args:
-        circuits (:class:`QuantumCircuit` or list(:class:`QuantumCircuit`)):
-                  Circuits to be twirled.
-        backend: (:class:`IBMQBackend`): backend the circtui will be run on, so
-                 that non-basis gates have appropriate calibrations added.
-        entangler_str (str): name of the entangling gate which will be twirled.
-        seeds (int or list(int)): number of seeds or range of randome seeds to use
-        validate (bool): verify Pauli twirls construct equivalent unitaries
+        circuits: Circuit or list of circuits to be twirled.
+        num_twirled_circuits: Number of twirled circuits to return for each input circuit.
+        seed: Seed for the pseudorandom number generator.
+
+    Returns:
+        If the input is a single circuit, then a list of circuits is returned.
+        If the input is a list of circuit, then a list of lists of circuits is returned.
     """
+    rng = parse_random_seed(seed)
     if isinstance(circuits, QuantumCircuit):
-        circuits = [circuits]
-
-    twirl_gates = get_twirl_gates(entangler_str)
-    inst_sched_map = backend.defaults().instruction_schedule_map
-
-    # find non-basis gates which are not defined by backend
-    extra_gate_defs = []
-    for twirl_pair in twirl_gates:
-        for twirl_gate in twirl_pair:
-            if twirl_gate.name not in inst_sched_map.instructions:
-                if twirl_gate.name not in extra_gate_defs:
-                    extra_gate_defs.append(twirl_gate.name)
-
-    all_twirled_circs = []
-    for circ in circuits:
-        dag = circuit_to_dag(circ)
-        twirled_circs = []
-        rng = np.random.default_rng(seed)
-
-        for _ in range(num_twirled_cicuits):
-            this_dag = deepcopy(dag)
-            runs = this_dag.collect_runs([entangler_str])
-            twirl_idxs = rng.integers(low=0, high=len(twirl_gates), size=len(runs))
-            for twirl_idx, run in enumerate(runs):
-                mini_dag = DAGCircuit()
-                p = QuantumRegister(2, 'p')
-                mini_dag.add_qreg(p)
-                mini_dag.apply_operation_back(twirl_gates[twirl_idxs[twirl_idx]][0], qargs=[p[0]])
-                mini_dag.apply_operation_back(twirl_gates[twirl_idxs[twirl_idx]][1], qargs=[p[1]])
-                mini_dag.apply_operation_back(run[0].op, qargs=[p[0], p[1]])
-                mini_dag.apply_operation_back(twirl_gates[twirl_idxs[twirl_idx]][0], qargs=[p[0]])
-                mini_dag.apply_operation_back(twirl_gates[twirl_idxs[twirl_idx]][1], qargs=[p[1]])
-
-                this_dag.substitute_node_with_dag(node=run[0], input_dag=mini_dag)
-
-            twirled_circs.append(dag_to_circuit(this_dag))
-
-            # add pulse gates (calibrations) for non-basis gates
-            if 'y' in extra_gate_defs:
-                for qubit in range(backend.configuration().num_qubits):
-                    with pulse.build('y gate for qubit '+str(qubit)) as sched:
-                        # def of YGate() in terms of XGate() and phase_offset
-                        with pulse.phase_offset(np.pi/2, DriveChannel(qubit)):
-                            x_gate = inst_sched_map.get('x', qubits=[qubit])
-                            pulse.call(x_gate)
-
-                    # for each Y twirl
-                    for t_circ in twirled_circs:
-                        t_circ.add_calibration('y', [qubit], sched)
-
-            if 'z' in extra_gate_defs:
-                continue # qiskit knows how to handle these
-
-        all_twirled_circs.append(twirled_circs)
-
-    return all_twirled_circs
+        return [_twirl_circuit(circuits, rng) for _ in range(num_twirled_circuits)]
+    return [
+        [_twirl_circuit(circuit, rng) for _ in range(num_twirled_circuits)]
+        for circuit in circuits
+    ]
 
 
-def get_twirl_gates(entangler_str: str) -> List[Gate]:
-    """
-    Return list of twirling gates for the entrangling gate to be twirled.
-    """
-    try:
-        return TWIRL_GATES[entangler_str]
-    except:
-        raise ValueError("Twirling gates not defined for entangler "+entangler_str)
+def _twirl_circuit(circuit: QuantumCircuit, rng: np.random.Generator) -> QuantumCircuit:
+    dag = circuit_to_dag(circuit)
+    gates_to_twirl = list(TWIRL_GATES)
+    for run in dag.collect_runs(gates_to_twirl):
+        for node in run:
+            twirl_gates = TWIRL_GATES[node.op.name]
+            (before0, before1), (after0, after1) = twirl_gates[
+                rng.integers(len(twirl_gates))
+            ]
+            mini_dag = DAGCircuit()
+            register = QuantumRegister(2)
+            mini_dag.add_qreg(register)
+            mini_dag.apply_operation_back(before0, [register[0]])
+            mini_dag.apply_operation_back(before1, [register[1]])
+            mini_dag.apply_operation_back(node.op, [register[0], register[1]])
+            mini_dag.apply_operation_back(after0, [register[0]])
+            mini_dag.apply_operation_back(after1, [register[1]])
+            dag.substitute_node_with_dag(node, mini_dag)
+    return dag_to_circuit(dag)
--- qiskit_research/utils/test_pauli_twirling.py
+++ qiskit_research/utils/test_pauli_twirling.py
@@ -10,64 +10,48 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Test dynamical decoupling."""
+"""Test Pauli twirling."""
 
 import unittest
 
-from qiskit import transpile
+import numpy as np
 from qiskit.circuit import Parameter, QuantumCircuit
 from qiskit.quantum_info import Operator
-from qiskit.test.mock import FakeMumbai
-from qiskit_research.utils.pauli_twirling import add_pauli_twirls, TWIRL_GATES
-
-import numpy as np
+from qiskit_research.utils.pauli_twirling import add_pauli_twirls
 
 
 class TestPauliTwirling(unittest.TestCase):
     """Test Pauli twirling."""
 
-    def test_add_pauli_twirls_to_rzx(self):
-        backend = FakeMumbai()
-        theta = Parameter('$\\theta$')
-        phi = Parameter('$\\phi')
+    def test_add_pauli_twirls(self):
+        rng = np.random.default_rng()
+
+        theta = Parameter("$\\theta$")
+        phi = Parameter("$\\phi")
+
         circuit = QuantumCircuit(3)
         circuit.h([1, 2])
         circuit.rzx(theta, 0, 1)
         circuit.h(1)
         circuit.rzx(phi, 1, 2)
-        circuit.h(2)
-        twirled_circs = add_pauli_twirls(circuit, backend, "rzx", 5, 1)
-
-        rng = np.random.default_rng()
-        param_bind = {}
-        for param in circuit.parameters:
-            param_bind[param] = rng.random()
-
-        circuit.assign_parameters(param_bind, inplace=True)
-        for t_circ in twirled_circs[0]:
-            t_circ.assign_parameters(param_bind, inplace=True)
-            self.assertNotEqual(t_circ, circuit)
-            self.assertTrue(Operator(t_circ).equiv(Operator(circuit)))
-
-    def test_add_pauli_twirls_to_rzz(self):
-        backend = FakeMumbai()
-        theta = Parameter('$\\theta$')
-        phi = Parameter('$\\phi')
-        circuit = QuantumCircuit(3)
-        circuit.h([1, 2])
         circuit.rzz(theta, 0, 1)
         circuit.h(1)
         circuit.rzz(phi, 1, 2)
         circuit.h(2)
-        twirled_circs = add_pauli_twirls(circuit, backend, "rzz", 5, 1)
+        twirled_circs = add_pauli_twirls(circuit, num_twirled_circuits=5)
+        more_twirled_circs = add_pauli_twirls(
+            [circuit], num_twirled_circuits=5, seed=1234
+        )
 
-        rng = np.random.default_rng()
-        param_bind = {}
-        for param in circuit.parameters:
-            param_bind[param] = rng.random()
-
+        param_bind = {param: rng.uniform(-10, 10) for param in circuit.parameters}
         circuit.assign_parameters(param_bind, inplace=True)
-        for t_circ in twirled_circs[0]:
+
+        for t_circ in twirled_circs:
+            t_circ.assign_parameters(param_bind, inplace=True)
+            self.assertNotEqual(t_circ, circuit)
+            self.assertTrue(Operator(t_circ).equiv(Operator(circuit)))
+
+        for t_circ in more_twirled_circs[0]:
             t_circ.assign_parameters(param_bind, inplace=True)
             self.assertNotEqual(t_circ, circuit)
             self.assertTrue(Operator(t_circ).equiv(Operator(circuit)))
