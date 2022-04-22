3d7a7516ebde4165bc82837589375d3ce316c43e
Kevin J. Sung <kevjsung@umich.edu>
2022-04-22 10:56:04 -0400

    implement pauli twirling as transpilation pass

--- qiskit_research/utils/pauli_twirling.py
+++ qiskit_research/utils/pauli_twirling.py
@@ -17,6 +17,9 @@ from qiskit.circuit import QuantumCircuit, QuantumRegister
 from qiskit.circuit.library import IGate, XGate, YGate, ZGate
 from qiskit.converters import circuit_to_dag, dag_to_circuit
 from qiskit.dagcircuit import DAGCircuit
+from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.transpiler import PassManager
+
 
 I = IGate()
 X = XGate()
@@ -52,6 +55,42 @@ def parse_random_seed(seed: Any) -> np.random.Generator:
     return np.random.default_rng(seed)
 
 
+class PauliTwirl(TransformationPass):
+    """Add Pauli twirls."""
+
+    def __init__(
+        self,
+        gates_to_twirl: Optional[Iterable[str]] = None,
+        seed: Any = None,
+    ):
+        if gates_to_twirl is None:
+            gates_to_twirl = TWIRL_GATES.keys()
+        self.gates_to_twirl = gates_to_twirl
+        self.rng = parse_random_seed(seed)
+        super().__init__()
+
+    def run(
+        self,
+        dag: DAGCircuit,
+    ) -> DAGCircuit:
+        for run in dag.collect_runs(list(self.gates_to_twirl)):
+            for node in run:
+                twirl_gates = TWIRL_GATES[node.op.name]
+                (before0, before1), (after0, after1) = twirl_gates[
+                    self.rng.integers(len(twirl_gates))
+                ]
+                mini_dag = DAGCircuit()
+                register = QuantumRegister(2)
+                mini_dag.add_qreg(register)
+                mini_dag.apply_operation_back(before0, [register[0]])
+                mini_dag.apply_operation_back(before1, [register[1]])
+                mini_dag.apply_operation_back(node.op, [register[0], register[1]])
+                mini_dag.apply_operation_back(after0, [register[0]])
+                mini_dag.apply_operation_back(after1, [register[1]])
+                dag.substitute_node_with_dag(node, mini_dag)
+        return dag
+
+
 def add_pauli_twirls(
     circuits: Union[QuantumCircuit, List[QuantumCircuit]],
     num_twirled_circuits: int = 1,
@@ -71,40 +110,10 @@ def add_pauli_twirls(
         If the input is a single circuit, then a list of circuits is returned.
         If the input is a list of circuit, then a list of lists of circuits is returned.
     """
-    if gates_to_twirl is None:
-        gates_to_twirl = TWIRL_GATES.keys()
-    rng = parse_random_seed(seed)
+    pass_manager = PassManager(PauliTwirl(gates_to_twirl=gates_to_twirl, seed=seed))
     if isinstance(circuits, QuantumCircuit):
-        return [
-            _twirl_circuit(circuits, gates_to_twirl, rng)
-            for _ in range(num_twirled_circuits)
-        ]
+        return [pass_manager.run(circuits) for _ in range(num_twirled_circuits)]
     return [
-        [
-            _twirl_circuit(circuit, gates_to_twirl, rng)
-            for _ in range(num_twirled_circuits)
-        ]
+        [pass_manager.run(circuit) for _ in range(num_twirled_circuits)]
         for circuit in circuits
     ]
-
-
-def _twirl_circuit(
-    circuit: QuantumCircuit, gates_to_twirl: Iterable[str], rng: np.random.Generator
-) -> QuantumCircuit:
-    dag = circuit_to_dag(circuit)
-    for run in dag.collect_runs(list(gates_to_twirl)):
-        for node in run:
-            twirl_gates = TWIRL_GATES[node.op.name]
-            (before0, before1), (after0, after1) = twirl_gates[
-                rng.integers(len(twirl_gates))
-            ]
-            mini_dag = DAGCircuit()
-            register = QuantumRegister(2)
-            mini_dag.add_qreg(register)
-            mini_dag.apply_operation_back(before0, [register[0]])
-            mini_dag.apply_operation_back(before1, [register[1]])
-            mini_dag.apply_operation_back(node.op, [register[0], register[1]])
-            mini_dag.apply_operation_back(after0, [register[0]])
-            mini_dag.apply_operation_back(after1, [register[1]])
-            dag.substitute_node_with_dag(node, mini_dag)
-    return dag_to_circuit(dag)
