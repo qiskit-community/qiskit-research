7911b68a0812643503131faec22f8a1d4f67c83d
Kevin J. Sung <kevjsung@umich.edu>
2022-06-02 09:31:50 +0200

    fix CI

--- qiskit_research/__init__.py
+++ qiskit_research/__init__.py
@@ -14,3 +14,12 @@
 Qiskit Research
 ===============
 """
+
+from importlib_metadata import version as metadata_version, PackageNotFoundError
+
+
+try:
+    __version__ = metadata_version("qiskit_research")
+except PackageNotFoundError:  # pragma: no cover
+    # package is not installed
+    pass
