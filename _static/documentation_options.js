5b8854db1e772f58d8ac76c0e9006c81aa931f44
kevinsung <kevinsung@users.noreply.github.com>
2022-07-18 21:55:39 +0000

    deploy: 46ab9fe5aded4fd15ce0bc3bcb997c90d874177a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gcf96561',
+    VERSION: '0.1.dev1+g46ab9fe',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
